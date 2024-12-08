e17b0b6711ee24b4e60f2bb993b49afdc43e4a8b
Sonali_167 <137919448+sonali167-coder@users.noreply.github.com>
2024-12-08 08:32:25 +0000

    2nd commit

---
+++ 01_basics/variable.js
@@ -0,0 +1,13 @@
+const accountId = 144553
+let accounrEmail = "cse.22bcsf70@silicon.ac.in"
+var accountPassword = "12345"
+accountCity = "Jaipur"
+
+// accountId = 2
+accounrEmail = "22bcsf70@silicon.ac.in"
+accountPassword = "67895"
+accountCity = "delhi"
+
+//console.log(accountId);
+
+console.table([accountId, accounrEmail, accountPassword, accountCity])
