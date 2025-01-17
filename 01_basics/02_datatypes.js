285e5e80498b345416c284b073b6e3f7d3723041
Sonali_167 <137919448+sonali167-coder@users.noreply.github.com>
2025-01-17 16:41:54 +0000

    2nd

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,13 @@
+// number ==> 2 to power 53
+//bigint
+//string ==> ""
+//boolean => true/false
+//null (standalone value) : empty value
+// undefined : variable declared but value not assigned
+//symbol : to find uniqueness
+
+//object
+
+console.log(typeof "Sonali");
+
+//typeof null = object 
