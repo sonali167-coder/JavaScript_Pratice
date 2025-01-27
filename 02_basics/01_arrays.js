311d5e24f2f7c949045f6668d7d450457428051c
Sonali_167 <137919448+sonali167-coder@users.noreply.github.com>
2025-01-27 15:14:46 +0000

    array01

---
+++ 02_basics/01_arrays.js
@@ -0,0 +1,22 @@
+const myArr = [0, 1, 2, 3, 4, 5]
+const myHeros = ["iron man", "thor"]
+
+const myArr2 = new Array(1, 2, 3, 4, 5)
+
+//console.log(myArr);
+
+
+//array methods 
+// myArr.push(6)
+// console.log(myArr);
+
+// myArr.push(7)
+// console.log(myArr);
+
+// myArr.pop()
+
+myArr.unshift(9)
+console.log(myArr)
+
+myArr.shift()
+console.log(myArr)
