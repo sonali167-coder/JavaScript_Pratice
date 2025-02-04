77da5a6295f6276295f18f501ebfa7dabffccda6
Sonali_167 <cse.22bcsf70@silicon.ac.in>
2025-02-04 10:45:50 +0530

    change

---
+++ 02_basics/02_comparison.js
@@ -0,0 +1,10 @@
+// console.log(2 > 1); //returns boolean value
+
+// console.log("2" > 1);
+
+console.log(null > 0)
+console.log(null >= 0)  //value conversio
+console.log(null == 0)
+
+// === check both value and data type 
+// == check only value 
